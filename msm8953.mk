577e6bea783a99f00df653670aa49991956d5e3a
TheGamerKing <eyadtamer2121@gmail.com>
2026-07-26 22:02:28 +0300

    msm8953-common: Add keylayouts for dt2w_pwrkey and s2w_pwrkey
    This is for the new DT2W and S2W software implementation in kernel, see https://github.com/TheGamerKing561/android_kernel_xiaomi_msm8953/commit/61ffc036364caf2ff4e9da7a4272329e83213a5b
    
    Signed-off-by: TheGamerKing <eyadtamer2121@gmail.com>

--- msm8953.mk
+++ msm8953.mk
@@ -174,6 +174,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
     $(LOCAL_PATH)/keylayout/gf3208.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gf3208.kl \
     $(LOCAL_PATH)/keylayout/goodix-ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/goodix-ts.kl \
+    $(LOCAL_PATH)/keylayout/dt2w_pwrkey.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/dt2w_pwrkey.kl \
+    $(LOCAL_PATH)/keylayout/s2w_pwrkey.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/s2w_pwrkey.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/gpio-keys.kl \
     $(LOCAL_PATH)/keylayout/ist30xx_ts_input.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ist30xx_ts_input.kl \
     $(LOCAL_PATH)/keylayout/msm8953-snd-card-mtp_Button_Jack.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/msm8953-snd-card-mtp_Button_Jack.kl \
